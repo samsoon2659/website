a92ced96d73a073deae66e7333eb38cb6bd53964
samsoon2659 <86094673+samsoon2659@users.noreply.github.com>
2021-11-21 22:27:22 +0900

    Add files via upload

---
+++ novel.php
@@ -0,0 +1,17 @@
+<?php
+	$conn = mysqli_connect("fdb32.awardspace.net", "3990532_samsoon2659", "kk1012kj0126", "3990532_samsoon2659");
+	$sql = "
+		select * from novel
+	";
+	$result = mysqli_query($conn, $sql);
+	if($result === false){
+		echo mysqli_error($conn);
+	}
+	$num = $result->num_rows;
+	
+	echo '<h1>소설</h1>';
+	while($row = mysqli_fetch_array($result)) {
+		echo '<h2>'.$row['name'].'</h2>';
+		echo $row['author'];
+	}
+?>
