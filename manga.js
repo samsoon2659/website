5d0f1d632ae6cd81ff3aebd04723336c8b96f10a
samsoon2659 <86094673+samsoon2659@users.noreply.github.com>
2021-11-22 22:55:14 +0900

    Update manga.js

--- manga.js
+++ manga.js
@@ -1,1 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwdJR6n3kG9-Qur6bFryhkI7my17uoPf0",
+  authDomain: "website-332910.firebaseapp.com",
+  projectId: "website-332910",
+  storageBucket: "website-332910.appspot.com",
+  messagingSenderId: "154490563083",
+  appId: "1:154490563083:web:3842bca2fe542abcdb350e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 document.write("hello");
