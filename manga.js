1147108d579e1d176a39629eba6ca597234ac510
samsoon2659 <86094673+samsoon2659@users.noreply.github.com>
2021-11-22 23:00:37 +0900

    Update manga.js

--- manga.js
+++ manga.js
@@ -16,4 +16,20 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+const db = getFirestore();
+var docRef = db.collection("manga").doc("bpWmPJwAlPNm9I4EhsWj");
+
+docRef.get().then((doc) => {
+    if (doc.exists) {
+        console.log("Document data:", doc.data());
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}).catch((error) => {
+    console.log("Error getting document:", error);
+});
+
+
+
 document.write("hello");
